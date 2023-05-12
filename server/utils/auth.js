207a5fe8b21d5c8eaf6c1a710919a64d070afd07
Rebecca Lehew <lehew316@gmail.com>
2023-05-12 10:17:03 -0500

    Update auth.js
    Updated server/utils/auth.js to GraphQL and Mongoose syntax

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +16,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      throw new Error("No authorization token!");
     }
 
     // verify token and get user data out of it
@@ -24,13 +24,14 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log(err);
+      throw new Errorr("Invalid authorization token!")
     }
 
-    // send to next endpoint
-    next();
+    // Returns the request object
+    return req;
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
