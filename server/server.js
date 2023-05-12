687d15ebc159758e6b5a69dcac2a74173adf670b
Rebecca Lehew <lehew316@gmail.com>
2023-05-12 10:23:05 -0500

    Update server.js
    - Updated server/server.js to require typeDefs and resolvers.
    - Removed references to routes.
    - Changed wording for console.log to remove API server phrase

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require("./schema");
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,7 +14,7 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -21,16 +22,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
-  
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
+      console.log(`Now listening on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
