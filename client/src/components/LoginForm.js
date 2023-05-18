ef43db8105d3c8a105be704e510190a3984013c2
Rebecca Lehew <lehew316@gmail.com>
2023-05-18 09:43:02 -0500

    Attempts to debug forms
    The search functionality is working...but the login/sign up forms are not. I'm not sure what the issues are...I may try a few more things before resubmitting.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -8,7 +8,7 @@ import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
+  const [validated, setValidated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
   const [loginUser] = useMutation(LOGIN_USER);
